15abbecfc3d3b3ecc1a4e9906220c769535d945f
kajal1107 <kajalyuvasoft298@gmail.com>
2021-01-15 11:52:04 +0530

    add post controller model

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	resources :articles do
-  		resources :comments
-  	end
+  	resources :comments
+  end
+  resources :posts do
+		resources :likes
+		resources :follows
+  end
   root 'welcome#index'
   get '/all_articles', to: 'articles#all_articles'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_095927) do
+ActiveRecord::Schema.define(version: 2020_11_20_113545) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,10 +29,27 @@ ActiveRecord::Schema.define(version: 2020_10_28_095927) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -63,4 +80,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_095927) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
